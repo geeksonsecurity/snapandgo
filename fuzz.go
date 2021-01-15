14ccc760a17fb4c66c995a302cd4f66e5d79ee2f
wizche <sergio.paganoni@gmail.com>
2021-01-15 22:17:12 +0100

    read/write breakpoint

--- fuzz.go
+++ fuzz.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
 	"syscall"
 )
+
+const mainAddr = 0x555555555303
+
+//ADDR_NO_RANDOMIZE disable randomization of VA space
+const ADDR_NO_RANDOMIZE = 0x0040000
 
 func debug() {
 	log.Printf("Debugging child!")
@@ -36,7 +42,26 @@ func explainWaitStatus(ws syscall.WaitStatus) string {
 	}
 }
 
+func read(pid int, addr uintptr, count int) []byte {
+	dword := make([]byte, count)
+	count, err := syscall.PtracePeekText(pid, addr, dword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Read %d bytes", count)
+	return dword
+}
+
+func write(pid int, addr uintptr, bytes []byte) int {
+	count, err := syscall.PtracePokeText(pid, addr, bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return count
+}
+
 func main() {
+	var err error
 	procAttr := syscall.ProcAttr{
 		Dir:   "",
 		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
@@ -46,6 +71,13 @@ func main() {
 		},
 	}
 
+	r1, _, errno := syscall.Syscall(syscall.SYS_PERSONALITY, ADDR_NO_RANDOMIZE, uintptr(0), uintptr(0))
+
+	if errno != 0 {
+		log.Printf("Failed to disable ASLR: %s", errno.Error())
+	}
+	log.Printf("Result %d", r1)
+
 	targetPid, err := syscall.ForkExec("./demo/target", []string{}, &procAttr)
 	if err != nil {
 		log.Fatal(err)
@@ -53,8 +85,18 @@ func main() {
 
 	log.Printf("Started target process with PID %d", targetPid)
 
+	// waiting stop at entrypoint
 	var wstat syscall.WaitStatus
-	//var regs syscall.PtraceRegs
+	_, err = syscall.Wait4(targetPid, &wstat, 0, nil)
+	fmt.Printf("Status: %s (%d)\n", explainWaitStatus(wstat), wstat)
+
+	// set breakpoint at main and let continue
+	// First byte in main
+	log.Printf("Read %s", hex.Dump(read(targetPid, mainAddr, 1)))
+
+	// Replace it with software breakpoint 0xCC
+	write(targetPid, mainAddr, []byte{0xCC})
+	syscall.PtraceCont(targetPid, 0)
 
 	for {
 		fmt.Println("Waiting..")
@@ -65,10 +107,15 @@ func main() {
 			fmt.Println(err)
 			break
 		}
+
+		if wstat.StopSignal() == 5 {
+			break
+		}
+
+		//log.Printf("Read %s", hex.Dump(read(targetPid, mainAddr, 1)))
 		syscall.PtraceCont(targetPid, 0)
 		//syscall.PtraceGetRegs(targetPid, &regs)
 		//fmt.Printf("syscall: %d\n", regs.Orig_rax)
-
 		//syscall.PtraceSyscall(targetPid, 0)
 	}
 }
